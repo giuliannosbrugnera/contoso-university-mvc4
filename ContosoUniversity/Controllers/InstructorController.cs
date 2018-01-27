c784a5fd6326a85f003cf7657c319889e2027ef9
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2018-01-27 19:38:41 -0200

    Creating the Instructor Controller and Views

---
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,152 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Instructor
+        public ActionResult Index(int? id, int? courseId)
+        {
+            var viewModel = new InstructorIndexData
+            {
+                Instructors = db.Instructors
+                    .Include(i => i.OfficeAssignment)
+                    .Include(i => i.Courses.Select(c => c.Department))
+                    .OrderBy(i => i.LastName)
+            };
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors.Single(_ => _.InstructorId == id.Value).Courses;
+            }
+
+            if (courseId != null)
+            {
+                ViewBag.CourseID = courseId.Value;
+                viewModel.Enrollments = viewModel.Courses.Single(_ => _.CourseId == courseId).Enrollments;
+            }
+
+            return View(viewModel);
+        }
+
+        // GET: Instructor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructor/Create
+        public ActionResult Create()
+        {
+            ViewBag.InstructorId = new SelectList(db.OfficeAssignments, "InstructorId", "Location");
+            return View();
+        }
+
+        // POST: Instructor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InstructorId,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorId = new SelectList(db.OfficeAssignments, "InstructorId", "Location", instructor.InstructorId);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorId = new SelectList(db.OfficeAssignments, "InstructorId", "Location", instructor.InstructorId);
+            return View(instructor);
+        }
+
+        // POST: Instructor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InstructorId,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorId = new SelectList(db.OfficeAssignments, "InstructorId", "Location", instructor.InstructorId);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: Instructor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var instructor = db.Instructors.Find(id);
+            db.Instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
